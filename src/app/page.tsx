089f8ed60831eb639ad1e085e282e6930d6d9002
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-02 22:49:25 +0530

    Initialize Next.js project with Tailwind CSS and ESLint configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Cursor from "@/components/Cursor";
+import Landingpage from "@/components/Landingpage";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full">
+      <Cursor />
+      <Landingpage />
+    </div>
+  );
+}
