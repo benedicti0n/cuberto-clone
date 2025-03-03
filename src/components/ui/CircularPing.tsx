6fac309847b9d1ac8b9ec09f86c2eee2b511489e
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-04 02:35:03 +0530

    Adjust padding and radius in CircularPing component for improved visual consistency

--- src/components/ui/CircularPing.tsx
+++ src/components/ui/CircularPing.tsx
@@ -4,9 +4,9 @@ import { SpinningText } from '../magicui/spinning-text'
 
 const CircularPing = () => {
     return (
-        <div className='bg-white rounded-full p-12 relative flex justify-center items-center'>
+        <div className='bg-white rounded-full p-10 relative flex justify-center items-center'>
             <Spinner key={"ring"} variant={"ring"} size={64} className='' />
-            <SpinningText className="text-xs text-black font-light-regular uppercase absolute" duration={10} radius={8}>
+            <SpinningText className="text-xs text-black font-light-regular uppercase absolute" duration={10} radius={7}>
                 Identity. Product. Design
             </SpinningText>
         </div>
