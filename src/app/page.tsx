15cf3afe314ebdf208458d513263599697b8c5cc
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-03 18:08:25 +0530

    Refactor Home component to import LandingPage subcomponents and enhance layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
 "use client"
 import Cursor from "@/components/Cursors/Cursor";
-import Landingpage from "@/components/Landingpage";
+import Landingpage from "@/components/LandingPage/Landingpage";
 import NavHeader from "@/components/NavHeader";
-import ShortPromo from "@/components/ShortPromo";
+import ShortPromo from "@/components/LandingPage/ShortPromo";
 import { useRef } from "react";
+import WhatWeDo from "@/components/LandingPage/WhatWeDo";
+import FeaturedProjects from "@/components/LandingPage/FeaturedProjects";
+import OurPhilosophy from "@/components/LandingPage/OurPhilosophy";
+import Resources from "@/components/LandingPage/Resources";
 
 export default function Home() {
 
   const stickyElement = useRef(null);
 
   return (
-    <div className="h-screen w-full">
+    <div className="w-full">
       <NavHeader ref={stickyElement} />
       <Landingpage />
       <ShortPromo />
-      <Cursor stickyElement={stickyElement} />
+      <WhatWeDo />
+      <FeaturedProjects />
+      <OurPhilosophy />
+      <Resources />
+      {/* <Cursor stickyElement={stickyElement} /> */}
     </div>
   );
 }
